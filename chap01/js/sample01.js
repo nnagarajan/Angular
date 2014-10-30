177b4134b3012b15a080429d4cf84cb0a61bb694
Naveen Nagarajan <nnagarasan@paypal.com>
2014-10-30 17:18:27 -0400

    Added sample files

---
+++ chap01/js/sample01.js
@@ -0,0 +1,27 @@
+(function () {
+
+    var app = angular.module('store', []);
+
+    var gem = [
+        {
+            name: "DodeChahedron",
+            price: 2.95,
+            description: "A DodeChahedron gem",
+            canPurchase: true,
+            soldOut: false
+        },
+        {
+            name: "Pentogonal Gem",
+            price: 5.95,
+            description: "A Pentogonal gem",
+            canPurchase: true,
+            soldOut: false
+        }
+    ]
+
+
+    app.controller('StoreController', function () {
+        this.product = gem;
+    });
+
+})();
